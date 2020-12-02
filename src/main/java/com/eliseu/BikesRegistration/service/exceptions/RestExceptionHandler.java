f665d98969863a24a35a79443a0378c9471f2554
eliseusbrito <eliseusbrito@gmail.com>
2020-12-02 14:58:03 -0300

    Personalizado e padronizado as Exceptions para mensagens internas do Spring

---
+++ src/main/java/com/eliseu/BikesRegistration/service/exceptions/RestExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.eliseu.BikesRegistration.service.exceptions;
+
+import com.eliseu.BikesRegistration.endpoint.exceptions.ErrorDetails;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex,
+                                                             Object body,
+                                                             HttpHeaders headers,
+                                                             HttpStatus status,
+                                                             WebRequest request) {
+        ErrorDetails errorDetails = ErrorDetails.Builder
+                .newBuilder()
+                .timestamp(new Date().getTime())
+                .status(HttpStatus.NOT_FOUND.value())
+                .title("Internal Exception")
+                .detail(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+        return new ResponseEntity<>(errorDetails, headers, status);
+    }
+
+}
